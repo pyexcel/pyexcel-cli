0672eacc3d21f3595a62747ee753b7721969b096
chfw <wangc_2011@hotmail.com>
2016-06-26 00:14:09 +0100

    avoid ods would help it succeed in pypy

--- tests/test_split.py
+++ tests/test_split.py
@@ -9,12 +9,12 @@ def test_simple_option():
     runner = CliRunner()
     file_fixture = os.path.join("tests", "fixtures", "multiple-sheets.xls")
     prefix = "output"
-    runner.invoke(split, ['--output-file-type', 'ods',
+    runner.invoke(split, ['--output-file-type', 'xlsx',
                           file_fixture, prefix])
     book = get_book(file_name=file_fixture)
     count = 0
     for sheet in book:
-        splitted_file = "%s_%s_%s.ods" % (prefix, sheet.name, count)
+        splitted_file = "%s_%s_%s.xlsx" % (prefix, sheet.name, count)
         count += 1
         written_sheet = get_sheet(file_name=splitted_file)
         eq_(str(sheet), str(written_sheet))
