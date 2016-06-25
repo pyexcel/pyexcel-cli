26b1891ad1e3593c2d3b91e99e0c062cf1b805df
chfw <wangc_2011@hotmail.com>
2016-06-25 23:56:43 +0100

    fix broken test case

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -18,16 +18,21 @@ def test_difference():
     test_fixture = os.path.join("tests", "fixtures", "transcode_simple.csv")
     test_fixture2 = os.path.join("tests", "fixtures", "multiple-sheets.xls")
     result = runner.invoke(diff, [test_fixture, test_fixture2])
-    fixing = ""
+    header = dedent("""
+    --- tests\\fixtures\\transcode_simple.csv
+
+    +++ tests\\fixtures\\multiple-sheets.xls
+
+    @@ -1 +1,15 @@""").strip('\n') + '\n'
     if os.name == "posix":
-        fixing = " "
-    expected = dedent("""
-    --- tests{0}fixtures{0}transcode_simple.csv{1}
+        header = dedent("""
+        --- tests/fixtures/transcode_simple.csv 
 
-    +++ tests{0}fixtures{0}multiple-sheets.xls{1}
+        +++ tests/fixtures/multiple-sheets.xls 
 
-    @@ -1 +1,15 @@
+        @@ -1,1 +1,15 @@""").strip('\n') + '\n'  # noqa
 
+    body = dedent("""
     +---pyexcel:Sheet 1---
      1,2,3
     +4,5,6
@@ -42,6 +47,7 @@ def test_difference():
     +O,P,Q
     +3,2,1
     +4,3,2
-    +---pyexcel---""").strip('\n') + '\n'
-    eq_(result.output, expected.format(os.sep, fixing))
+    +---pyexcel---
+    """)
+    eq_(result.output, header + body)
     eq_(result.exit_code, 1)
