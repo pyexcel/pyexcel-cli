e10e7141c292d2b475bf308ac7882d82defabe56
chfw <wangc_2011@hotmail.com>
2016-06-26 23:06:58 +0100

    improve test coverage

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -13,6 +13,34 @@ def test_no_difference():
     eq_(result.exit_code, 0)
 
 
+def test_stdin_option():
+    runner = CliRunner()
+    test_fixture2 = os.path.join("tests", "fixtures", "multiple-sheets.xls")
+    result = runner.invoke(diff,
+                           ["--source-file-type", "csv",
+                            "-", test_fixture2],
+                           input='1,2,3')
+    body = dedent("""
+    +---pyexcel:Sheet 1---
+     1,2,3
+    +4,5,6
+    +7,8,9
+    +---pyexcel---
+    +---pyexcel:Sheet 2---
+    +X,Y,Z
+    +1,2,3
+    +4,5,6
+    +---pyexcel---
+    +---pyexcel:Sheet 3---
+    +O,P,Q
+    +3,2,1
+    +4,3,2
+    +---pyexcel---
+    """)
+    assert result.output.endswith(body)
+    eq_(result.exit_code, 1)
+
+
 def test_difference():
     runner = CliRunner()
     test_fixture = os.path.join("tests", "fixtures", "transcode_simple.csv")
--- tests/test_split.py
+++ tests/test_split.py
@@ -19,3 +19,24 @@ def test_simple_option():
         written_sheet = get_sheet(file_name=splitted_file)
         eq_(str(sheet), str(written_sheet))
         os.unlink(splitted_file)
+
+
+def test_stdin_option():
+    runner = CliRunner()
+    file_fixture = os.path.join("tests", "fixtures", "multiple-sheets.xls")
+    input_stream = open(file_fixture, "rb")
+    prefix = "output"
+    runner.invoke(split,
+                  ['--source-file-type', "xls",
+                   '--output-file-type', 'xlsx',
+                   '-', prefix],
+                  input=input_stream.read())
+    book = get_book(file_name=file_fixture)
+    count = 0
+    for sheet in book:
+        splitted_file = "%s_%s_%s.xlsx" % (prefix, sheet.name, count)
+        count += 1
+        written_sheet = get_sheet(file_name=splitted_file)
+        eq_(str(sheet), str(written_sheet))
+        os.unlink(splitted_file)
+    input_stream.close()
