0972b3d38bd7364262bb32f4181c86ae5f167332
chfw <wangc_2011@hotmail.com>
2017-03-02 17:17:54 +0000

    sync with commons

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'pyexcel-cli'
 AUTHOR = 'C.W.'
-VERSION = '0.0.3'
+VERSION = '0.0.2'
 EMAIL = 'wangc_2011 (at) hotmail.com'
 LICENSE = 'New BSD'
 ENTRY_POINTS = {
--- test.sh
+++ test.sh
@@ -1,4 +1,2 @@
-
-
 pip freeze
 nosetests --with-cov --cover-package pyexcel_cli --cover-package tests --with-doctest --doctest-extension=.rst README.rst tests docs/source pyexcel_cli && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
